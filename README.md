48685b6c9a719eaea3b470689db424b40f67b2c1
Thorsten Zoerner <me@thorsten-zoerner.com>
2023-07-18 00:58:17 +0200

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # nr-meross-cloud
 Node-RED Node to get Energy Data of  Meross SmartPlug  (like MSS310)
+
+During development of [Corrently Current](https://github.com/energychain/corrently-current) we wanted  to get energy data from smart plugs in. Thats's why this Node exists. It is designed mainly to be used in the embedded environment of a [Corrently Edge](https://github.com/energychain/corrently-edge) Node-RED instance.
+
+## Disclaimer
+**All product and company names or logos are trademarks™ or registered® trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them or any associated subsidiaries! This personal project is maintained in spare time and has no business goal.**
+**MEROSS is a trademark of Chengdu Meross Technology Co., Ltd.**
+
+## Maintainer / Imprint
+
+<addr>
+STROMDAO GmbH  <br/>
+Gerhard Weiser Ring 29  <br/>
+69256 Mauer  <br/>
+Germany  <br/>
+  <br/>
++49 6226 968 009 0  <br/>
+  <br/>
+kontakt@stromdao.com  <br/>
+  <br/>
+Handelsregister: HRB 728691 (Amtsgericht Mannheim)
+</addr>
+
---
+++ nr-meross-cloud.js
@@ -0,0 +1,84 @@
+module.exports = function(RED) {
+    function Reader(config) {
+        const MerossCloud = require('meross-cloud');
+        RED.nodes.createNode(this,config);
+
+        const options = {
+            email: config.username,
+            password: config.password,
+            logger: ()=> {},
+            localHttpFirst: true, 
+            onlyLocalForGet: true, 
+            timeout: 15000 
+        };
+        const node = this;
+
+        const meross = new MerossCloud(options);
+        console.log("Finished Setup Meross");
+
+        meross.on('connected', (deviceId) => {
+            console.log(deviceId + ' connected');
+        });
+        
+        meross.on('close', (deviceId, error) => {
+            console.log(deviceId + ' closed: ' + error);
+        });
+        
+        meross.on('error', (deviceId, error) => {
+            console.log(deviceId + ' error: ' + error);
+        });
+        
+        meross.on('reconnect', (deviceId) => {
+            console.log(deviceId + ' reconnected');
+        });
+        
+        meross.on('data', (deviceId, payload) => {
+            console.log(deviceId + ' data: ' + JSON.stringify(payload));
+        });
+        
+        meross.connect((error) => {
+            console.log('connect error: ' + error);
+        });
+
+        meross.on('deviceInitialized', (deviceId, deviceDef, device) => {
+            node.status({ fill: "red", shape: "dot", text: "connecting"});
+            device.on('connected', () => {
+                node.status({ fill: "yellow", shape: "dot", text: "connecting"});
+                setInterval(function() {
+                   
+                        device.getControlElectricity((err, res) => {
+                            try {
+                            res.electricity.device = deviceDef;
+    
+                            node.send({
+                                topic:deviceId,
+                                payload:res.electricity
+                            });
+                            
+                            node.status({ fill: "green", shape: "dot", text: ""+(res.electricity.current/10)+"W "+(res.electricity.voltage/10)+"V"});
+                            } catch(e) {
+                                node.status({ fill: "red", shape: "dot", text: "Exception:"+e});
+                            }
+                        });
+                   
+                },5000);
+                
+             });    
+        });
+        
+       /*
+        node.on('input', async function(msg) {
+            try {
+                
+                node.send(msg);
+                node.status({ fill: "green", shape: "dot", text: "Local:"+latest.localprice+" / National:"+latest.marketprice});
+            }
+           catch(e) {
+                node.status({ fill: "red", shape: "dot", text: "Unable to retrieve from API"});
+                console.log(e);
+           }
+        });
+        */
+    }
+    RED.nodes.registerType("MerossEnergy",Reader);
+}
